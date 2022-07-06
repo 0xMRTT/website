e4369fb77fd36ea1d06578b2e608228250ed0f9b
0xMRTT <0xMRTT@tuta.io>
2022-07-06 17:22:38 +0200

    Using [slug].vue

---
+++ pages/blog/[slug].vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const route = useRoute()
+const { data } = await useAsyncData(route.path, () => queryContent(route.path).findOne())
+</script>
+
+<template>
+  <article>
+    <ContentRenderer :value="data">
+      <template #empty>        
+        <p>No content found.</p>      
+      </template>
+      <h1>{{ data.title }}</h1>
+      <ContentRendererMarkdown :value="data" />
+    </ContentRenderer>
+  </article>
+</template>
