25b008a47e204c4f4ae6c65166dd5d58cb938e65
0xMRTT <0xMRTT@tuta.io>
2022-07-06 17:25:58 +0200

    Adding disaply of title

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -5,11 +5,14 @@ const { data } = await useAsyncData(route.path, () => queryContent(route.path).f
 
 <template>
   <article>
+    <h1>{{ data.title }}</h1>
     <ContentRenderer :value="data">
       <template #empty>        
         <p>No content found.</p>      
       </template>
-      <h1>{{ data.title }}</h1>
+      <template #not-found>
+        <p>No content found.</p>  
+      </template>
       <ContentRendererMarkdown :value="data" />
     </ContentRenderer>
   </article>
