12627d4d218e543899bcf4fa569f798bb16ea412
0xMRTT <0xMRTT@tuta.io>
2022-07-07 00:25:29 +0200

    Add icon light

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,37 @@
 <template>
     <div>
+        <div class="navbar bg-base-300 rounded-box">
+            <div class="flex-1 px-2 lg:flex-none">
+                <a class="text-lg font-bold">0xMRTT</a>
+            </div>
+            <div class="flex justify-end flex-1 px-2">
+                <div class="flex items-stretch">
+                    <a class="btn btn-ghost rounded-btn">Button</a>
+                    <div class="dropdown dropdown-end">
+                        <label tabindex="0" class="btn btn-ghost rounded-btn">Dropdown</label>
+                        <ul tabindex="0" class="menu dropdown-content p-2 shadow bg-base-100 rounded-box w-52 mt-4">
+                            <li v-for="theme of themes" :key="theme"><a>{{ theme }}</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div>
-            <h1>Color mode: {{ $colorMode.value }}</h1>
-            <select class="select w-full max-w-xs" v-model="colorMode.preference">
-                <option disabled selected>Theme</option>
-                <option v-for="theme of themes" :key="theme">{{ theme }}</option>
-            </select>
+            <ul>
+                <li v-for="color of ['system', 'light', 'dark', 'sepia']" :key="color" :class="{
+                    preferred: !$colorMode.unknown && color === $colorMode.preference,
+                    selected: !$colorMode.unknown && color === $colorMode.value,
+                }">
+                    <component :is="`icon-${color}`" @click="$colorMode.preference = color" />
+                </li>
+            </ul>
+            <p>
+                <ColorScheme placeholder="..." tag="span">
+                    Color mode: <b>{{ $colorMode.preference }}</b>
+                    <span v-if="$colorMode.preference === 'system'">&nbsp;(<i>{{ $colorMode.value }}</i> mode
+                        detected)</span>
+                </ColorScheme>
+            </p>
         </div>
 
         <div>
