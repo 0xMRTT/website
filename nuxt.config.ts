5cd4d6232ad3d6d4554e5d41321eddaed74136cf
0xMRTT <0xMRTT@tuta.io>
2022-07-06 23:41:11 +0200

    Add colormode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ import { defineNuxtConfig } from 'nuxt'
 export default defineNuxtConfig({
   modules: [
     '@nuxt/content',
+    "@nuxtjs/color-mode",
     '@nuxtjs/tailwindcss'
   ],
   components: true
