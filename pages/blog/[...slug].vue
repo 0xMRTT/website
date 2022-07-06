3c3f2c7f3d9a9b284f9a06aa27540627f0e03578
0xMRTT <0xMRTT@tuta.io>
2022-07-06 16:58:37 +0200

    Adding slug

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,5 +1,16 @@
+<script setup lang="ts">
+const { data } = await useAsyncData('{{$route.path}}', () => queryContent('{{$route.path}}').findOne())
+</script>
+
 <template>
-  <main>
-    <ContentDoc />
-  </main>
+  <article>
+    {{ $route.path }}
+    <ContentRenderer :value="data">
+      <template #empty>        
+        <p>No content found.</p>      
+      </template>
+      <h1>{{ data.title }}</h1>
+      <ContentRendererMarkdown :value="data" />
+    </ContentRenderer>
+  </article>
 </template>
