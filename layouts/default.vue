9dcd11ff00a03bf48fc3758c8f354d6e323a6404
0xMRTT <0xMRTT@tuta.io>
2022-07-07 00:11:14 +0200

    Adding daisy ui to layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,11 +2,9 @@
     <div>
         <div>
             <h1>Color mode: {{ $colorMode.value }}</h1>
-            <select v-model="$colorMode.preference">
-                <option value="system">System</option>
-                <option value="light">Light</option>
-                <option value="dark">Dark</option>
-                <option value="sepia">Sepia</option>
+            <select class="select w-full max-w-xs" v-model="colorMode.preference">
+                <option disabled selected>Theme</option>
+                <option v-for="theme of themes" :key="theme">{{ theme }}</option>
             </select>
         </div>
 
@@ -18,22 +16,37 @@
 
 <script setup>
 const colorMode = useColorMode()
+const themes = [
+    'light',
+    'dark',
+    'cupcake',
+    'bumblebee',
+    'emerald',
+    'corporate',
+    'synthwave',
+    'retro',
+    'cyberpunk',
+    'valentine',
+    'halloween',
+    'garden',
+    'forest',
+    'aqua',
+    'lofi',
+    'pastel',
+    'fantasy',
+    'wireframe',
+    'black',
+    'luxury',
+    'dracula',
+    'cmyk',
+    'autumn',
+    'business',
+    'acid',
+    'lemonade',
+    'night',
+    'coffee',
+    'winter',
+];
 console.log(colorMode.preference)
 </script>
 
-<style>
-body {
-    background-color: #fff;
-    color: rgba(0, 0, 0, 0.8);
-}
-
-.dark-mode body {
-    background-color: #091a28;
-    color: #ebf4f1;
-}
-
-.sepia-mode body {
-    background-color: #f1e7d0;
-    color: #433422;
-}
-</style>
