60dc20fa9cb8638a23e714bbd24f9c9df9d38a1f
0xMRTT <0xMRTT@tuta.io>
2022-07-07 00:37:42 +0200

    Add show of color

--- components/ColorModePicker.vue
+++ components/ColorModePicker.vue
@@ -9,6 +9,7 @@
           selected: !$colorMode.unknown && color === $colorMode.value,
         }"
       >
+      {{color}}
         <component
           :is="`icon-${color}`"
           @click="$colorMode.preference = color"
@@ -25,41 +26,3 @@
     </p>
   </div>
 </template>
-
-<style scoped>
-ul {
-  list-style: none;
-  padding: 0;
-  margin: 0;
-}
-ul li {
-  display: inline-block;
-  padding: 5px;
-}
-p {
-  margin: 0;
-  padding-top: 5px;
-  padding-bottom: 20px;
-}
-.feather {
-  position: relative;
-  top: 0px;
-  cursor: pointer;
-  padding: 7px;
-  background-color: var(--bg-secondary);
-  border: 2px solid var(--border-color);
-  margin: 0;
-  border-radius: 5px;
-  transition: all 0.1s ease;
-}
-.feather:hover {
-  top: -3px;
-}
-.preferred .feather {
-  border-color: var(--color-primary);
-  top: -3px;
-}
-.selected .feather {
-  color: var(--color-primary);
-}
-</style>
