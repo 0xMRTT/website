5f36cebbbb00d950ebf38e7c9e30654270902c4c
0xMRTT <0xMRTT@tuta.io>
2022-07-07 00:38:38 +0200

    Remove useless items

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,13 +2,12 @@
     <div>
         <div class="navbar bg-base-300 rounded-box">
             <div class="flex-1 px-2 lg:flex-none">
-                <a class="text-lg font-bold">daisyUI</a>
+                <a class="text-lg font-bold">0xMRTT</a>
             </div>
             <div class="flex justify-end flex-1 px-2">
                 <div class="flex items-stretch">
-                    <a class="btn btn-ghost rounded-btn">Button</a>
                     <div class="dropdown dropdown-end">
-                        <label tabindex="0" class="btn btn-ghost rounded-btn">Dropdown</label>
+                        <label tabindex="0" class="btn btn-ghost rounded-btn">Color</label>
                         <ul tabindex="0" class="menu dropdown-content p-2 shadow bg-base-100 rounded-box w-52 mt-4">
                             <li v-for="color of ['system', 'light', 'dark', 'sepia']" :key="color" :class="{
                                 preferred: !$colorMode.unknown && color === $colorMode.preference,
