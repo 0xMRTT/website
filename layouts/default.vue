a8a697b00d6a3b987258d30b4c2ca5239f143466
0xMRTT <0xMRTT@tuta.io>
2022-07-07 01:22:28 +0200

    Add avatar

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,13 @@
     <div>
         <div class="navbar bg-base-300 rounded-box">
             <div class="flex-1 px-2 lg:flex-none">
-                <a class="text-lg font-bold">0xMRTT</a>
+                <NuxtLink to="/">
+                <div class="avatar">
+                    <div class="w-11 rounded-full">
+                        <img src="https://avatars.githubusercontent.com/0xMRTT" />
+                    </div>
+                </div>
+                </NuxtLink>
             </div>
             <div class="flex justify-end flex-1 px-2">
                 <div class="flex items-stretch">
