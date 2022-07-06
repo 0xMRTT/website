e83436a19b38238f315b40bb59c36f77ac85a8c8
0xMRTT <0xMRTT@tuta.io>
2022-07-07 00:37:30 +0200

    Add navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,25 @@
 <template>
     <div>
-        <div>
-            COLOR
-            <ColorModePicker />
-            MODE
+        <div class="navbar bg-base-300 rounded-box">
+            <div class="flex-1 px-2 lg:flex-none">
+                <a class="text-lg font-bold">daisyUI</a>
+            </div>
+            <div class="flex justify-end flex-1 px-2">
+                <div class="flex items-stretch">
+                    <a class="btn btn-ghost rounded-btn">Button</a>
+                    <div class="dropdown dropdown-end">
+                        <label tabindex="0" class="btn btn-ghost rounded-btn">Dropdown</label>
+                        <ul tabindex="0" class="menu dropdown-content p-2 shadow bg-base-100 rounded-box w-52 mt-4">
+                            <li v-for="color of ['system', 'light', 'dark', 'sepia']" :key="color" :class="{
+                                preferred: !$colorMode.unknown && color === $colorMode.preference,
+                                selected: !$colorMode.unknown && color === $colorMode.value,
+                            }">
+                                <p @click="$colorMode.preference = color">{{ color }}</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
 
         <div>
@@ -16,4 +32,3 @@
 const colorMode = useColorMode()
 console.log(colorMode.preference)
 </script>
-
