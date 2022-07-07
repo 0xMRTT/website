c5787778f4f262a6509809a99d682046620bcc5c
0xMRTT <0xMRTT@tuta.io>
2022-07-07 11:15:40 +0200

    Add config for postcss

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,5 +57,11 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
   }
 }
