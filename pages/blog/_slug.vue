666a98e42c582636b123fd9d2e5e13122f04654a
0xMRTT <0xMRTT@tuta.io>
2022-07-07 11:14:48 +0200

    Add tailwind css

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -27,3 +27,16 @@
   </article>
 </template>
 
+<style>
+  .nuxt-content h2 {
+    font-weight: bold;
+    font-size: 28px;
+  }
+  .nuxt-content h3 {
+    font-weight: bold;
+    font-size: 22px;
+  }
+  .nuxt-content p {
+    margin-bottom: 20px;
+  }
+</style>
