9ed201b90e26103e71d8c9ce784dace122c9b4aa
0xMRTT <0xMRTT@tuta.io>
2022-07-07 11:38:39 +0200

    Add display of color mode

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,7 +14,7 @@
                     </div>
                 </div>
 
-            </div>blog/hello#displaying-content
+            </div>
 
             <div class="navbar-center hidden lg:flex">
                 <ul class="menu menu-horizontal p-0">
@@ -74,7 +74,15 @@
                 </div>
             </div>
         </div>
-
+        <div>
+    <h1>Color mode: {{ $colorMode.value }}</h1>
+    <select v-model="$colorMode.preference">
+      <option value="system">System</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+      <option value="sepia">Sepia</option>
+    </select>
+  </div>
         <div class="py-5 px-7">
             <Nuxt />
         </div>
@@ -110,8 +118,3 @@
 
     </div>
 </template>
-
-<script setup>
-const colorMode = useColorMode()
-console.log(colorMode.preference)
-</script>
