5c5377bcad3d442081cf266c09890c05a20b3dd6
0xMRTT <0xMRTT@tuta.io>
2022-07-07 11:10:33 +0200

    Add method

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -4,7 +4,14 @@
       const article = await $content('articles', params.slug).fetch()
 
       return { article }
+    },
+    methods: {
+    formatDate(date) {
+      const options = { year: 'numeric', month: 'long', day: 'numeric' }
+      return new Date(date).toLocaleDateString('en', options)
     }
+ }
+
   }
 </script>
 
