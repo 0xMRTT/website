19e0a19aab2dda8a091f584c639ff8d587dd39b2
0xMRTT <0xMRTT@tuta.io>
2022-07-07 11:19:30 +0200

    Add TOC

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,50 +1,60 @@
 <script>
-  export default {
+export default {
     async asyncData({ $content, params }) {
-      const article = await $content('articles', params.slug).fetch()
+        const article = await $content('articles', params.slug).fetch()
 
-      return { article }
+        return { article }
     },
     methods: {
-    formatDate(date) {
-      const options = { year: 'numeric', month: 'long', day: 'numeric' }
-      return new Date(date).toLocaleDateString('en', options)
+        formatDate(date) {
+            const options = { year: 'numeric', month: 'long', day: 'numeric' }
+            return new Date(date).toLocaleDateString('en', options)
+        }
     }
- }
 
-  }
+}
 </script>
 
 
 <template>
-  <article>
-    <h1>{{ article.title }}</h1>
-    <p>{{ article.description }}</p>
-    <img :src="article.img" :alt="article.alt" />
-    <p>Article last updated: {{ formatDate(article.updatedAt) }}</p>
+    <article>
+        <nav>
+            <ul>
+                <li v-for="link of article.toc" :key="link.id">
+                    <NuxtLink :to="`#${link.id}`">{{ link.text }}</NuxtLink>
+                </li>
+            </ul>
+        </nav>
 
-    <nuxt-content :document="article" />
-  </article>
+        <h1>{{ article.title }}</h1>
+        <p>{{ article.description }}</p>
+        <img :src="article.img" :alt="article.alt" />
+        <p>Article last updated: {{ formatDate(article.updatedAt) }}</p>
+
+        <nuxt-content :document="article" />
+    </article>
 </template>
 
 <style>
-  .nuxt-content h2 {
+.nuxt-content h2 {
     font-weight: bold;
     font-size: 28px;
-  }
-  .nuxt-content h3 {
-    font-weight: bold;
-    font-size: 22px;
-  }
-  .nuxt-content p {
-    margin-bottom: 20px;
-  }
-  .icon.icon-link {
-  background-image: url('~assets/svg/hashtag.svg');
-  display: inline-block;
-  width: 20px;
-  height: 20px;
-  background-size: 20px 20px;
 }
 
+.nuxt-content h3 {
+    font-weight: bold;
+    font-size: 22px;
+}
+
+.nuxt-content p {
+    margin-bottom: 20px;
+}
+
+.icon.icon-link {
+    background-image: url('~assets/svg/hashtag.svg');
+    display: inline-block;
+    width: 20px;
+    height: 20px;
+    background-size: 20px 20px;
+}
 </style>
