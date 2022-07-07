8d0f751df45031ae77c5799d8421d4b492c6449b
0xMRTT <0xMRTT@tuta.io>
2022-07-07 11:39:15 +0200

    Remove debug

--- layouts/default.vue
+++ layouts/default.vue
@@ -75,14 +75,6 @@
             </div>
         </div>
         <div>
-    <h1>Color mode: {{ $colorMode.value }}</h1>
-    <select v-model="$colorMode.preference">
-      <option value="system">System</option>
-      <option value="light">Light</option>
-      <option value="dark">Dark</option>
-      <option value="sepia">Sepia</option>
-    </select>
-  </div>
         <div class="py-5 px-7">
             <Nuxt />
         </div>
